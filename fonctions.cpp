002fe6a5279159a7f322907f174e3cfc40ea1c47
nounoursheureux <schtroumps31@gmail.com>
2014-03-26 19:22:08 +0100

    Supprimé quelques fichiers

--- fonctions.cpp
+++ fonctions.cpp
@@ -3,15 +3,3 @@ using namespace sf;
 #include "Classes.hpp"
 #include <iostream>
 
-int checkCollision(Ball ball, Terrain terrain) {
-	FloatRect posBalle = ball.returnPos();
-	FloatRect* murs = terrain.returnMurs();
-	if (posBalle.intersects(murs[0])) 
-		return 1;
-	if (posBalle.intersects(murs[1]))
-		return 1;
-	if (posBalle.intersects(murs[2]) || posBalle.intersects(murs[3]))
-		return 2;
-	else 
-		return 0; 
-}
